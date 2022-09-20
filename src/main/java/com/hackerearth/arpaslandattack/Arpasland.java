a08ec5267c5df19fd3cff6779b81d2dc060c30d4
Santiago Lopez <santiagolopezpardo@gmail.com>
2022-09-19 23:08:28 -0500

    fixed code to complete challenge https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/cartag-948c2b02/?

--- src/main/java/com/hackerearth/arpaslandattack/Arpasland.java
+++ src/main/java/com/hackerearth/arpaslandattack/Arpasland.java
@@ -4,46 +4,58 @@ import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.function.BiFunction;
 import java.util.function.Predicate;
 
 public class Arpasland {
 
-    public static void main(String[] args) throws IOException {
+    private static boolean allTrue (ArrayList<Boolean> values) {
+        for (boolean value : values) {
+            if (!value)
+                return false;
+        }
+        return true;
+    }
+
+        public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedOutputStream bos = new BufferedOutputStream(System.out);
 
         String code = br.readLine();
         StringBuilder codeStringBuilder = new StringBuilder(code);
-        boolean isCodeValid = false;
-        boolean wasPreviousCharaNumber = false;
+        ArrayList<Boolean> isCodeValid = new ArrayList<>(codeStringBuilder.length());
+        boolean repeatDo = true;
+        boolean wasPreviousCharANumberAndNOTADash = false;
 
-        Predicate<Character> isVowel = x -> x.equals("A") || x.equals("E") || x.equals("I") || x.equals("O") || x.equals("U") || x.equals("Y");
-        Predicate<Character> isNumber = x -> x.equals("0") || x.equals("1") || x.equals("2") || x.equals("3") || x.equals("4") || x.equals("5") || x.equals("6") || x.equals("7") || x.equals("8") || x.equals("9");
+        Predicate<Character> isVowel = x -> x.equals('A') || x.equals('E') || x.equals('I') || x.equals('O') || x.equals('U') || x.equals('Y');
+        Predicate<Character> isNumber = x -> x.equals('0') || x.equals('1') || x.equals('2') || x.equals('3') || x.equals('4') || x.equals('5') || x.equals('6') || x.equals('7') || x.equals('8') || x.equals('9');
         BiFunction<Integer, Integer, Boolean> isSumEven = (Integer T, Integer U) -> (T + U) % 2 == 0;
 
         for (int i = 0; i < codeStringBuilder.length(); i++) {
             Character currentChar = codeStringBuilder.charAt(i);
-            if (isCodeValid) {
-                if (currentChar != '-') {
-                    if (wasPreviousCharaNumber == false) {
-                        if (isNumber.test(currentChar)) {
-                            wasPreviousCharaNumber = true;
-                        } else {
-                            isCodeValid = isVowel.test(currentChar);
-                        }
-                    } else if (isNumber.test(currentChar)) {
-                        isCodeValid = isSumEven.apply((int) currentChar, (int) codeStringBuilder.charAt(i - 1));
-                    } else {
-                        isCodeValid = isVowel.test(currentChar);
-                    }
+            isCodeValid.add(true);
+            //You need to check if every all the return values are true every time the loop runs
+
+            if (currentChar != '-') {
+                if (wasPreviousCharANumberAndNOTADash == false) {
+                    if (isNumber.test(currentChar)) {
+                        wasPreviousCharANumberAndNOTADash = true;
+                    } else isCodeValid.set(i, !isVowel.test(currentChar));
+                } else if (isNumber.test(currentChar)) {
+                    isCodeValid.set(i,isSumEven.apply((int) currentChar, (int) codeStringBuilder.charAt(i - 1)));
+                } else {
+                    wasPreviousCharANumberAndNOTADash = false;
+                    isCodeValid.set(i,!isVowel.test(currentChar));
                 }
-            } else {
-                isCodeValid = false;
-            }
+            } else wasPreviousCharANumberAndNOTADash = false;
+
+
         }
 
-        if (isCodeValid) {
+
+
+        if (allTrue(isCodeValid)) {
             bos.write("valid".getBytes());
         } else {
             bos.write("invalid".getBytes());
