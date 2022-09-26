d6aa7325c2790e303f7540dc31e0c9d3d18a3970
Santiago Lopez <santiagolopezpardo@gmail.com>
2022-09-25 20:04:13 -0500

    added comment explaining to problem https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/cartag-948c2b02/

--- src/main/java/com/hackerearth/arpaslandattack/Arpasland.java
+++ src/main/java/com/hackerearth/arpaslandattack/Arpasland.java
@@ -9,16 +9,29 @@ import java.util.function.BiFunction;
 import java.util.function.Predicate;
 
 public class Arpasland {
+    /*
+    Taken from: https://www.hackerearth.com/practice/basic-programming/input-output/basics-of-input-output/practice-problems/algorithm/cartag-948c2b02/
 
-    private static boolean allTrue (ArrayList<Boolean> values) {
+    Arpasland has surrounded by attackers. A truck enters the city. The driver claims the load is food and medicine from Iranians.
+    Ali is one of the soldiers in Arpasland. He doubts about the truck, maybe it's from the siege.
+    He knows that a tag is valid if the sum of every two consecutive digits of it is even and its letter is not a vowel. Determine if the tag of the truck is valid or not.
+    We consider the letters "A","E","I","O","U","Y" to be vowels for this problem.
+    Input Format
+
+    The first line contains a string of length 9. The format is "DDXDDD-DD", where D stands for a digit (non zero) and X is an uppercase english letter.
+    Output Format
+
+    Print "valid" (without quotes) if the tag is valid, print "invalid" otherwise (without quotes)
+    */
+
+    private static boolean allTrue(ArrayList<Boolean> values) {
         for (boolean value : values) {
-            if (!value)
-                return false;
+            if (!value) return false;
         }
         return true;
     }
 
-        public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedOutputStream bos = new BufferedOutputStream(System.out);
 
@@ -43,17 +56,13 @@ public class Arpasland {
                         wasPreviousCharANumberAndNOTADash = true;
                     } else isCodeValid.set(i, !isVowel.test(currentChar));
                 } else if (isNumber.test(currentChar)) {
-                    isCodeValid.set(i,isSumEven.apply((int) currentChar, (int) codeStringBuilder.charAt(i - 1)));
+                    isCodeValid.set(i, isSumEven.apply((int) currentChar, (int) codeStringBuilder.charAt(i - 1)));
                 } else {
                     wasPreviousCharANumberAndNOTADash = false;
-                    isCodeValid.set(i,!isVowel.test(currentChar));
+                    isCodeValid.set(i, !isVowel.test(currentChar));
                 }
             } else wasPreviousCharANumberAndNOTADash = false;
-
-
         }
-
-
 
         if (allTrue(isCodeValid)) {
             bos.write("valid".getBytes());
