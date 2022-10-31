3c5bbc4f3a6c90d98adfd712682b8048c350f7e8
Santiago Lopez <santiagolopezpardo@gmail.com>
2022-10-30 22:46:32 -0500

    solved challenge https://leetcode.com/problems/find-pivot-index/

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+_
+/src/main/java/com/leetcode/twosum/Solution.java,e\8\e886adb7103890a9072669d10c2d00ca6a86d364
--- src/main/java/com/leetcode/pivotindex/Solution.java
+++ src/main/java/com/leetcode/pivotindex/Solution.java
@@ -1,43 +1,19 @@
 package com.leetcode.pivotindex;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 public class Solution {
     public int pivotIndex(int[] nums) {
-        Integer result = null;
+        int leftSum = 0, sum = Arrays.stream(nums).sum();
+
         for (int i = 0; i < nums.length; i++) {
-            List<Integer> right = new ArrayList<>();
-            List<Integer> left = new ArrayList<>();
-            int leftSum = 0, rightSum = 0;
-
-            for (int j = 0; j < i; j++) {
-                left.add(nums[j]);
+            if (i != 0) {
+                leftSum += nums[i - 1];
             }
-
-            for (int num : left) {
-                leftSum += num;
-            }
-
-            for (int j = i + 1; j < nums.length; j++) {
-                if (j > nums.length) {
-                    right.add(0);
-                } else {
-                    right.add(nums[j]);
-                }
-            }
-
-            for (int num : right) {
-                rightSum += num;
-            }
-
-            if (leftSum == rightSum) {
-                result = i;
-                return result;
-            } else {
-                result = -1;
+            if (leftSum == sum - leftSum - nums[i]) {
+                return i;
             }
         }
-        return result;
+        return -1;
     }
 }
